3919058bfe0380ec2b02d406a4734ea6ea312805
Fahad Shoaib <f.shoaib1580@gmail.com>
2023-08-17 15:33:27 -0400

    ContactPage

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,18 +1,26 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+// import DisplayCard from './DisplayCard';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
+import { selectFeaturedPartner } from '../partner/partnersSlice';
 
 const DisplayList = () => {
-    const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+    const items = [
+        selectFeaturedCampsite(),
+        selectFeaturedPromotion(),
+        selectFeaturedPartner()
+    ];
 
     return (
         <Row>
             {items.map((item, idx) => {
                 return (
-                    <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
-                    </Col>
+                    item && (
+                        <Col md className='m-1' key={idx}>
+                            <AnimatedDisplayCard item={item} />
+                        </Col>
+                    )
                 );
             })}
         </Row>
