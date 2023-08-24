f4cc73d95cfac2fa07d4e80084a2b1745e15250b
Fahad Shoaib <f.shoaib1580@gmail.com>
2023-08-23 23:31:02 -0400

    Add Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comment/commentsSlice';
 import { partnersReducer } from '../features/partner/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import logger from 'redux-logger';
+import { getDefaultNormalizer } from '@testing-library/react';
 
 export const store = configureStore({
   reducer: {
@@ -11,4 +13,5 @@ export const store = configureStore({
     promotions: promotionsReducer,
     partners: partnersReducer
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
