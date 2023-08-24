c92fcbc2caaa0fddc159a7094380b135b921e93d
Fahad Shoaib <f.shoaib1580@gmail.com>
2023-08-23 23:15:13 -0400

    Redux for All

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
+import { commentsReducer } from '../features/comment/commentsSlice';
+import { partnersReducer } from '../features/partner/partnersSlice';
+import { promotionsReducer } from '../features/promotions/promotionsSlice';
 
 export const store = configureStore({
   reducer: {
     campsites: campsitesReducer,
+    comments: commentsReducer,
+    promotions: promotionsReducer,
+    partners: partnersReducer
   },
 });
