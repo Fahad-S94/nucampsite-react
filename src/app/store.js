7cec447d4c867fd9382bbda1040120f47ccce5d6
Fahad Shoaib <f.shoaib1580@gmail.com>
2023-08-24 00:10:10 -0400

    Fixed Middleware commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,9 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comment/commentsSlice';
 import { partnersReducer } from '../features/partner/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
 import logger from 'redux-logger';
-import { getDefaultNormalizer } from '@testing-library/react';
 
 export const store = configureStore({
   reducer: {
